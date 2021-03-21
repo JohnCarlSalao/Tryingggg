67b75367824e989564f407faa6daaab0e3df85f6
John Carl Salao <salaojohn4@gmail.com>
2021-03-21 10:02:37 +0800

    Update App.js

--- App.js
+++ App.js
@@ -6,21 +6,24 @@ export default function App() {
   return (
     <ScrollView>
     <View style={styles.container}>
-    <Text style ={{height: 10, borderColor: 'gray', borderWidth: 10}} > Who is the best rapper </Text>
-      <TextInput style = {{height: 100, borderColor:'red', borderWidth: 1}} placeholder = "Thebe Kgotsitsile" value ="Inputted Value"/>
-      <Text style ={{height: 10, borderColor: 'gray', borderWidth: 15}} >Earl Sweatshirt Albums</Text>
-      <Text style ={{height: 20, borderColor: 'gray', borderWidth: 15}} >IDLSIGO</Text>
+    <Text style > Earl Sweatshirt </Text>
+      <TextInput style = {{textAlign: 'center', borderStyle: 'dotted', borderWidth: 20,  }} placeholder = "Thebe Kgotsitsile" value ="Inputted Value"/>
+      <Text>Earl Sweatshirt Albums</Text>
+      <Text>IDLSIGO</Text>
       <Image style = {{width: 400, height: 400}}source = {{uri: "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/I_Don%27t_Like_Shit%2C_I_Don%27t_Go_Outside_An_Album_by_Earl_Sweatshirt.jpg/220px-I_Don%27t_Like_Shit%2C_I_Don%27t_Go_Outside_An_Album_by_Earl_Sweatshirt.jpg"}}></Image>
       <Image style = {{width: 1022, height: 1533}} source= {{uri: "https://i.pinimg.com/originals/72/e3/4f/72e34f4e5b003fd2f5acf594a742c087.jpg"}} ></Image>
-      <Text style ={{height: 20, borderColor: 'gray', borderWidth: 30}} >Doris</Text>
+      <View style={styles.new}>
+      <Text style={styles.new} >Doris</Text>
       <Image style = {{width: 400, height: 400}}source = {{uri: "https://upload.wikimedia.org/wikipedia/en/4/40/Earl_Sweatshirt_Doris.jpg"}}></Image>
-      <Text style ={{height: 10, borderColor: 'blue', borderWidth: 15}} >SRS</Text>
+      <Text style={styles.new} >SRS</Text>
+      </View>
       <Image style = {{width: 400, height: 400}}source = {{uri: "https://upload.wikimedia.org/wikipedia/en/d/db/Some_Rap_Songs.jpg"}}></Image>
       
     </View>
-    <Text style ={{height: 15, borderColor: 'gray', borderWidth: 10}} > Who is the best rapper </Text>
-      <TextInput style = {{height: 15, borderColor:'red', borderWidth: 1}} placeholder = "Thebe Kgotsitsile" value ="Inputted Value"/>
-
+    <View style={styles.new}>
+    <Text> Earl Sweatshirt </Text>
+      <TextInput style={styles.new}  placeholder = "Thebe Kgotsitsile" value ="Earl Sweatshirt"/>
+      </View>
     </ScrollView>
   );
 }
@@ -32,4 +35,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  new:{
+    backgroundColor: 'pink',
+    padding:20,
+    textAlign: 'center'
+  
+    
+  },
+
+middle: {
+  flex: 0.3,
+  backgroundColor: "beige",
+  borderWidth: 5,
+  alignItems: 'center'
+  
+}
+  
 });
